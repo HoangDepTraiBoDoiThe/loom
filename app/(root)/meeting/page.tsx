b7d153d58a396516af2be02802c8c8c5a97a0b3e
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-04-07 14:13:05 +0700

    Add route pages

--- app/(root)/meeting/page.tsx
+++ app/(root)/meeting/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Meeting</h1>
+    </section>
+  );
 };
 
 export default page;
--- app/(root)/recordings/page.tsx
+++ app/(root)/recordings/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const page = (props: Props) => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">Recording</h1>
     </section>
   );
 };
