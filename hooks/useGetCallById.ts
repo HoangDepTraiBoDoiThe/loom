1b39ffa62f2fa0f10c8569935f6aceab6cdee279
HoangDepTraiBoDoiThe <123256016+HoangDepTraiBoDoiThe@users.noreply.github.com>
2024-04-10 17:07:56 +0700

    Setup useGetCallById hook

--- hooks/useGetCallById.ts
+++ hooks/useGetCallById.ts
@@ -1,1 +1,32 @@
-export const useGetCallById = (id: string | string[]) => {};
+"use client";
+
+import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useEffect, useState } from "react";
+
+export const useGetCallById = (
+  id: string | string[]
+): { call?: Call; isLoading?: boolean } => {
+  const [call, setCall] = useState<Call>();
+  const [isLoading, setIsLoading] = useState(true);
+  const videoClient = useStreamVideoClient();
+
+  useEffect(() => {
+    console.log("videoClient", videoClient);
+    if (!videoClient) return;
+    const loadCall = async () => {
+      const { calls } = await videoClient.queryCalls({
+        filter_conditions: {
+          id,
+        },
+      });
+      console.log("calls", calls);
+      if (calls.length > 0) setCall(calls[0]);
+      setIsLoading(false);
+    };
+    loadCall();
+  }, [id, videoClient]);
+
+  if (!videoClient) return {};
+
+  return { call, isLoading };
+};
